59387402eb1f9ba74e2ec4880417da29e11729b4
Nikolai Panteleev <panteleew.kolya@gmail.com>
2023-09-28 12:54:34 +0900

    modified2:   routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -10,8 +10,8 @@ router.get('/me', getUser);
 
 router.patch('/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    email: Joi.string().email().required(),
+    name: Joi.string().min(2).max(30),
+    email: Joi.string().email(),
   }),
 }), updateUser);
 
